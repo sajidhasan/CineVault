85acc71f62c28c13ab2b2c1bfb82d2e608f6e36d
sajidhasan <md.sajidhasan@yahoo.com>
2023-09-07 22:03:59 +0100

    initial commit

---
+++ src/test/java/uk/sajidhasan/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.sajidhasan.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
